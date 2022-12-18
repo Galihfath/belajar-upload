b8dba1c2c2595c8f24d296606c3e88056b401231
galih fathurrochman <112954226+Galihfath@users.noreply.github.com>
2022-12-18 22:48:51 +0700

    upload

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "Netlify.com" // TODO: replace this
+const studentName = "Galih Fathurrochman" // TODO: replace this
+const studentId = "FE3094106" // TODO: replace this
+
+export { url, studentName, studentId }
