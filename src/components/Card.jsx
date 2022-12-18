a31db4448c9d83e48e14277ad52141c721fd10a4
galih fathurrochman <112954226+Galihfath@users.noreply.github.com>
2022-12-18 23:24:16 +0700

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,11 +5,19 @@ const Card = ({ photo, deletePhoto }) => {
     <div className="card">
       <img className="card-image" src={photo.imageUrl} alt={photo.id} />
       <div className="card-content">
-        <p className="captions" data-testid="photo-caption">{photo.captions}</p>
+        <p className="captions" data-testid="photo-caption">
+          {photo.captions}
+        </p>
         <button className="edit-btn">
           <Link to={`${photo.id}`}>Edit</Link>
         </button>
-        <button className="delete-btn" onClick={() => deletePhoto(photo.id)}>Delete</button>
+        <button
+          data-testid="delete-btn"
+          className="delete-btn"
+          onClick={() => deletePhoto(photo.id)}
+        >
+          Delete
+        </button>
       </div>
     </div>
   );
